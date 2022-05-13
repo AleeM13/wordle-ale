11ae477e5b958614893af6454344514db7354916
Alejandro Miranda <ale.miranda4503@gmail.com>
2022-05-13 00:04:25 -0300

    Nuevo

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var width = 5;
 var row = 0;
 var col = 0;
 var GameOver = false
-const wordAle = 'LAZOS'
+const wordAle = 'MECHA'
 const keys = [
     'Q',
     'W',
@@ -126,7 +126,7 @@ const checkRow = () => {
             if (currentRow >= 5) {
                 isGameOver = false
                 showMessage('Ups, perdiste mal ahí :(')
-                showMessage('La palabra era "LAZOS"')
+                showMessage('La palabra era "MECHA"')
                 return
             }
             if (currentRow < 5) {
