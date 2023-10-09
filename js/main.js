487425722ab65a08fe730d61ac795eae855a1e74
Alejandro Miranda <88655777+AleeM13@users.noreply.github.com>
2023-10-09 00:02:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var width = 5;
 var row = 0;
 var col = 0;
 var GameOver = false
-const wordAle = 'ACTOR'
+const wordAle = 'DROGA'
 const keys = [
     'Q',
     'W',
@@ -126,7 +126,7 @@ const checkRow = () => {
             if (currentRow >= 5) {
                 isGameOver = false
                 showMessage('Ups, perdiste mal ahí :(')
-                showMessage('La palabra era "ACTOR"')
+                showMessage('La palabra era "DROGA"')
                 return
             }
             if (currentRow < 5) {
